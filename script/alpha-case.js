e7b6bee8992981da6de34030a04e88b10df5cd2e
Noman <alnoman047@gmail.com>
2024-02-15 10:38:08 +0600

    added functionality

--- script/alpha-case.js
+++ script/alpha-case.js
@@ -1,3 +1,22 @@
+// function play(){
+//     const homeSection = document.getElementById('home');
+//     homeSection.classList.add('hidden');
+
+//     const playgroundSection = document.getElementById('playground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
-    console.log('play start now');
+    hideElementById('home');
+    showElementById('playground');
+    continueGame()
 }
