fcc8baa1babec9463947a2c264368bc30d469075
Noman <alnoman047@gmail.com>
2024-02-16 00:15:15 +0600

    more functionality added

--- script/alpha-case.js
+++ script/alpha-case.js
@@ -6,6 +6,28 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+    }
+    else {
+        console.log('you missed. you lost a life');
+    }
+}
+// capture keyboard button press
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
 
