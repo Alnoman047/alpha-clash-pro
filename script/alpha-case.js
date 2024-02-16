59e86722702fb770ae6d3030fa2079d6f53db01e
Noman <alnoman047@gmail.com>
2024-02-16 22:19:24 +0600

    completed the game

--- script/alpha-case.js
+++ script/alpha-case.js
@@ -8,9 +8,12 @@
 
 function handleKeyboardButtonPress(event){
     const playerPressed = event.key;
-    console.log('player pressed', playerPressed);
+    // stop game if press Esc
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
 
-
+    // key player is expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
@@ -19,10 +22,51 @@ function handleKeyboardButtonPress(event){
 
     // check matched or not
     if(playerPressed === expectedAlphabet){
-        console.log('you get a point');
+        console.log('you got a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore );
+
+
+
+
+
+        // -------------------
+        // update score
+        // 1.get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        
+
+        // // 2. increase the score by 1
+        const newScore = currentScore + 1;
+        // // 3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet); 
+        continueGame();
     }
     else {
         console.log('you missed. you lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current-life', updateLife);
+
+        if(updateLife === 0){
+            gameOver();
+        }
+        // // get the current life number
+        // const currentLifeElement = document.getElementById('current-life')
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // // reduce the life count
+        // const newLife = currentLife - 1;
+        // //  display the updated life count
+        // currentLifeElement.innerText = newLife;
     }
 }
 // capture keyboard button press
@@ -38,7 +82,29 @@ function continueGame(){
 }
 
 function play(){
+    // hide everything show only the playground
     hideElementById('home');
+    hideElementById('final-score');
     showElementById('playground');
+
+    // reset score and life 
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame()
+}
+
+function gameOver(){
+    hideElementById('playground');
+    showElementById('final-score');
+    // update final score
+    // get the final score 
+
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('score-point', lastScore);
+
+    // clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+
 }
